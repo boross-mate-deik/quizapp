fad22fcfbffdb286af4cfd1cf71d320f02b2fe2b
Boross Máté <boross-mate@mailbox.unideb.hu>
2025-09-28 19:16:35 +0200

    Impl: added basic admin site functionality.

--- backend/quizapp/admin.py
+++ backend/quizapp/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Answer
 
-admin.site.register(Question)
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question Information", {"fields": ["question_text", "multiple_answer", "category"]}),
+        ("Date information", {"fields": ["pub_date"]})
+    ]
+    inlines = [AnswerInline]
+    list_display = ["question_text", "multiple_answer", "category", "pub_date"]
+    list_filter = ["multiple_answer", "category"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
