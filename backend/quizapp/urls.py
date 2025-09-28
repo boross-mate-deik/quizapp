2890ef81e30b7fd36ebe2eaa3e58ee5d55222a58
Boross Máté <boross-mate@mailbox.unideb.hu>
2025-09-28 18:55:19 +0200

    Impl: added basic solution system

--- backend/quizapp/urls.py
+++ backend/quizapp/urls.py
@@ -6,5 +6,6 @@ app_name = "quizapp"
 urlpatterns = [
     path("", views.home, name="home"),
     path("<int:question_id>/", views.solve, name="solve"),
+    path("<int:question_id>/answer/", views.answer, name="answer"),
     path("<int:question_id>/results/", views.results, name="results"),
 ]
--- backend/quizapp/views.py
+++ backend/quizapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.db.models import F
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+from .models import Question, Answer
 
 
 def home(request):
@@ -12,8 +14,42 @@ def home(request):
 
 def solve(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "quizapp/solve.html", {"question": question})
+    return render(request,
+                  "quizapp/solve.html",
+                  {
+                      "question": question,
+                      "multiple_answer": question.multiple_answer,
+                  })
+
+def answer(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    selected_ids = request.POST.getlist("answer")
+
+    if not selected_ids:
+        return render(
+            request,
+            "quizapp/solve.html",
+            {
+                "question": question,
+                "multiple_answer": question.multiple_answer,
+                "error_message": "You didn't select any answers.",
+            },
+        )
+
+    request.session["selected_answers"] = selected_ids
+
+    return HttpResponseRedirect(reverse("quizapp:results", args=(question.id,)))
 
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    selected_ids = request.session.get("selected_answers", [])
+    selected_ids = [int(x) for x in selected_ids]
+    answers = question.answer_set.all()
+    return render(request,
+                  "quizapp/results.html",
+                  {
+                      "question": question,
+                      "answers": answers,
+                      "selected_ids": selected_ids,
+                  })
